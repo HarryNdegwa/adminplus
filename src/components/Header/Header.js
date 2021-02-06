215eb2294a2aa684abc17cc2fda7c56f4b56ef5b
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-02-06 23:27:35 +0300

    Added Header component and bs5

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const HeaderWrapper = styled.div`
+  background-color: "red";
+`;
+
+function Header(props) {
+  return <div></div>;
+}
+
+export default Header;
