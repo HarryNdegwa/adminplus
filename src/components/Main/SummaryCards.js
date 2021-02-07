f1a3c48a3517e71ba1f5442163b32e8019899dd5
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-02-07 20:58:06 +0300

    Finalized summary cards

--- src/components/Main/SummaryCards.js
+++ src/components/Main/SummaryCards.js
@@ -1,5 +1,9 @@
 import React from "react";
 import styled from "styled-components";
+import { FaCartArrowDown } from "react-icons/fa";
+import { GiShoppingBag } from "react-icons/gi";
+import { BsGraphUp } from "react-icons/bs";
+import { BiDollar } from "react-icons/bi";
 
 const SummaryCardsWrapper = styled.div`
   .summary-card {
@@ -8,21 +12,17 @@ const SummaryCardsWrapper = styled.div`
 
   > div .card1 {
     background: linear-gradient(135deg, #22c1b5, #3be897);
-    height: 200px;
   }
 
   > div .card2 {
     background: linear-gradient(135deg, #a358fd, #e06ada);
-    height: 200px;
   }
   > div .card3 {
     background: linear-gradient(135deg, #f58762, #ff9f3f);
-    height: 200px;
   }
   > div .card4 {
     background-color: #319ff4;
     background: linear-gradient(135deg, #319ff4, #7dbfef);
-    height: 200px;
   }
 `;
 
@@ -32,24 +32,52 @@ function SummaryCards(props) {
       <div className="col-md-6 col-lg-3">
         <div className="p-4 p-lg-3 card1 summary-card">
           <h5>Orders Received</h5>
+          <div className="d-flex justify-content-between my-4">
+            <span>
+              <FaCartArrowDown style={{ fontSize: "40px" }} />
+            </span>
+            <h5 style={{ fontSize: "35px", fontWeight: "bold" }}>358</h5>
+          </div>
+          <h6 className="my-2">18% higher than last month</h6>
         </div>
       </div>
 
       <div className="col-md-6 col-lg-3">
         <div className="p-4 p-lg-3 card2 summary-card">
           <h5 className="fs-lg-6">Completed Orders</h5>
+          <div className="d-flex justify-content-between my-4">
+            <span>
+              <GiShoppingBag style={{ fontSize: "40px" }} />
+            </span>
+            <h5 style={{ fontSize: "35px", fontWeight: "bold" }}>865</h5>
+          </div>
+          <h6 className="my-2">21% higher than last month</h6>
         </div>
       </div>
 
       <div className="col-md-6 col-lg-3">
         <div className="p-4 p-lg-3 card3 summary-card">
           <h5>New Orders</h5>
+          <div className="d-flex justify-content-between my-4">
+            <span>
+              <BsGraphUp style={{ fontSize: "40px" }} />
+            </span>
+            <h5 style={{ fontSize: "35px", fontWeight: "bold" }}>128</h5>
+          </div>
+          <h6 className="my-2">37% higher than last month</h6>
         </div>
       </div>
 
       <div className="col-md-6 col-lg-3">
         <div className="p-4 p-lg-3 card4 summary-card">
           <h5>Total Earnings</h5>
+          <div className="d-flex justify-content-between my-4">
+            <span>
+              <BiDollar style={{ fontSize: "40px" }} />
+            </span>
+            <h5 style={{ fontSize: "35px", fontWeight: "bold" }}>$25698</h5>
+          </div>
+          <h6 className="my-2">10% higher than last month</h6>
         </div>
       </div>
     </SummaryCardsWrapper>
